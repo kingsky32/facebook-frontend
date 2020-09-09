9d5635cedf6f7f149067e855ac616db51a169478
seungju <kingsky32@gmail.com>
2020-09-09 19:36:12 +0900

    Edit Queries

--- src/Routes/Login/LoginQueries.js
+++ src/Routes/Login/LoginQueries.js
@@ -6,29 +6,6 @@ export const LOG_IN = gql`
   }
 `;
 
-export const CREATE_ACCOUNT = gql`
-  mutation createAccount(
-    $username: String!
-    $email: String!
-    $password: String!
-    $firstName: String
-    $lastName: String
-  ) {
-    createAccount(
-      username: $username
-      email: $email
-      password: $password
-      firstName: $firstName
-      lastName: $lastName
-    )
-  }
-`;
-export const CONFIRM_SECRET = gql`
-  mutation confirmSecret($secret: String!, $email: String!) {
-    confirmSecret(secret: $secret, email: $email)
-  }
-`;
-
 export const LOCAL_LOG_IN = gql`
   mutation logUserIn($token: String!) {
     logUserIn(token: $token) @client
