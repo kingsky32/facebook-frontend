9b85438525694834cbb95068c818860dc4c35847
seungju <kingsky32@gmail.com>
2020-09-13 01:01:25 +0900

    Update IP

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "http://192.168.219.107:4000",
+  uri: "http://192.168.219.106:4000",
   clientState: {
     defaults,
     resolvers
